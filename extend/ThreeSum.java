b7f16e0f857a21eca9fcd4c0c44c4ac78b9bfe94
Chen Kaun Lin <qscf6541@gmail.com>
2020-08-21 12:56:54 +0800

    extend

---
+++ extend/ThreeSum.java
@@ -0,0 +1,15 @@
+
+public abstract class ThreeSum {
+	public abstract int T_sum(int[] A);
+}
+public static void main(String[] args) 
+	{
+		int[] A = {-1, 2, 3, -2, 1};
+		
+	}
+	
+	@Override
+	public int T_sum(int[] A) {
+		
+		return 0;
+	}
